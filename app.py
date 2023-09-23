10403d0e8cf13fbd930194da22a2720e4992296e
subash-013 <114496754+subash-013@users.noreply.github.com>
2023-09-23 22:49:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,31 +4,42 @@ from main import satelite_image_detection
 
 # Streamlit UI
 # Set the company logo image path
-cols = st.columns([1, 2])
-logo_image_path = "logo.jpeg"
+
+
+
+
+
+
+
+cols = st.columns([1,1])
+
+logo_image_path = "QAIG.jpg"
 image1 = Image.open(logo_image_path)
-image1 = image1.resize((200,150))
+# image1 = image1.resize((300,150))
 # Display the company logo
 cols[0].image(image1)
 
 
-title_style = """
-    <style>
-        /* Change the color to red (you can use any valid CSS color value) */
-        .title {
-            color: red;
-        }
-    </style>
-"""
+# title_style = """
+#     <style>
+#         /* Change the color to red (you can use any valid CSS color value) */
+#         .title {
+#             color: red;
+#         }
+#     </style>
+# """
 
-# Display the title with the custom style
-cols[1].markdown(f'<h1 class="title">QUANTUM AI GLOBAL</h1>', unsafe_allow_html=True)
+# # Display the title with the custom style
+# cols[1].markdown(f'<h1 class="title">OBJECT DETECTION</h1>', unsafe_allow_html=True)
+
+satra_logo_image_path = "satra_logo.jpg"
+image2 = Image.open(satra_logo_image_path)
+image2 = image2.resize((270,342))
+cols[1].image(image2)
 
 
 
-
-
-st.title("Satelitie Image Detection")
+st.title("OBJECT DETECTION")
 # st.write("Upload a .tif image for display:")
 
 # Upload a .tif image
@@ -39,22 +50,24 @@ if uploaded_image is not None:
     image_type = uploaded_image.type
     if image_type == 'image/tiff':
         tif_image = Image.open(uploaded_image)
-        if tif_image.mode == 'P':
+        if tif_image.mode != 'RGB':
             tif_image = tif_image.convert('RGB')
         # Convert and save it as a .jpg image
         tif_image.save("input_image.jpg")
         uploaded_image = Image.open("input_image.jpg")
     else:
-        uploaded_image.save("input_image.jpg")
+       uploaded_image.save("input_image.jpg")
         
     # Display the uploaded image
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
     if st.button("Detect"):
         rows = 7
         columns = 13
+        labels = {0:'building',1:'rail_network',2:'agriculture_land',3:'trees',4:'water_bodies',5:'road_network'}
+        confidences = {0:[],1:[],2:[],3:[],4:[],5:[]}
         st.session_state.prediction = True
         if st.session_state.prediction == True:
-            satelite_image_detection(rows,columns,"input_image.jpg")
+            satelite_image_detection(rows,columns,"input_image.jpg",labels,confidences)
     
         jpeg_image = Image.open("merged_image.jpg")
         # Save the TIFF image
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from ultralytics import YOLO
 import os
 import shutil
-from ultralytics import YOLO
 import cv2
 import numpy as np
+import openpyxl
 
-
-def detect_object(image_path):
+def detect_object(image_path,confidences):
         # image_path = 'image1x2.jpg'
         
         # Initialize the YOLO model
@@ -14,7 +13,7 @@ def detect_object(image_path):
         model = YOLO(model_path)
 
         # Perform the prediction
-        model.predict(
+        results = model.predict(
             source="splitted_data/"+image_path,
             conf=0.25,
             save=True,
@@ -24,7 +23,8 @@ def detect_object(image_path):
 
         # Display the output image
         # print(results[0])
-
+        result_object = results[0]
+        conf = compute_confidence(result_object,confidences)
 
         # Specify the source file path (the image you want to move)
         source_file = 'results/p1/'+image_path
@@ -50,7 +50,13 @@ def detect_object(image_path):
             shutil.rmtree(folder_path)
         else:
             print(f"The folder '{folder_path}' does not exist.")
-        return
+        return conf
+
+
+def compute_confidence(result,confidences):
+    for i in result.boxes:
+        confidences[int(i.cls[0])].append(float(i.conf[0]))
+    return confidences
 
 
 def split_image(rows,columns,parts,path):
@@ -124,9 +130,14 @@ def merge_image(rows,columns,parts):
   return
 
 
-def satelite_image_detection(rows,columns,path):
+def satelite_image_detection(rows,columns,path,labels,confidences):
     # rows = 7
     # columns = 13
+    folder_path = 'results/p1'
+        # Optionally, check if the folder exists before deleting it
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        
     n = rows*columns
     parts = []
     for i in range(n):
@@ -138,13 +149,44 @@ def satelite_image_detection(rows,columns,path):
     
     #detecting the objects
     for i in parts:
-        detect_object(i)
+        confidences = detect_object(i,confidences)
 
     #merging the image parts
     merge_image(rows,columns,parts)
     
     clean_folder("predicted")
     clean_folder("splitted_data")
+    save_to_Excel(confidences,labels)
+    return
+
+
+def save_to_Excel(confidences,labels):
+    final_conf = {}
+    for i in confidences.keys():
+        if(len(confidences[i])>0):
+            final_conf[labels[i]]=round((sum(confidences[i])/len(confidences[i])), 4)
+    workbook = openpyxl.load_workbook('my_data.xlsx')
+
+    # Select a specific sheet in the workbook (change 'Sheet1' to your sheet name)
+    sheet = workbook['Sheet1']
+    
+    num_rows = sheet.max_row
+    
+
+    data = []
+    for i in final_conf.keys():
+        num_rows += 1
+        data.append(["ID"+str(num_rows),i,final_conf[i]])
+    
+    for row in data:
+        sheet.append(row)
+
+    # Save the workbook to a file
+    workbook.save('my_data.xlsx')
+
+    # Close the workbook
+    workbook.close()
+    
     return
 
 
@@ -159,5 +201,9 @@ def clean_folder(folder_path):
 
 # rows = 7
 # columns = 13
-# image_path = 'samp1.jpg'
-# satelite_image_detection(rows,columns,image_path)
+# image_path = 'input_image.jpg'
+# # labels = {0:'building',1:'rail_network',2:'agriculture_land',3:'trees',4:'water_bodies',5:'road_network'}
+# # confidences = {0:[],1:[],2:[],3:[],4:[],5:[]}
+# ls = {0:'building',1:'rail_network',2:'agriculture_land',3:'trees',4:'water_bodies',5:'road_network'}
+# cs = {0:[],1:[],2:[],3:[],4:[],5:[]}
+# satelite_image_detection(rows,columns,image_path,ls,cs)
